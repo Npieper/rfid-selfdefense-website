c7e029694fc2bdd1cc3c8ea6465ac2c83240f2a4
Niklas Pieper <pieper.niklas.gm@gmail.com>
2019-09-08 15:25:22 +0200

    Add Homepage buttons

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,33 @@ const mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const connection = mysql.createConnection({
+  /*const connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-02.cleardb.net',
+    user: 'b525fb3beb7abe',
+    database: 'heroku_f88e5c34407b619',
+    password: '431db98c'
+  })
+
+
+  const queryString = "SELECT * FROM user";
+  connection.query(queryString, (err, rows, fields) => {
+    if(err) {
+      console.log("Failed to query for users: " + err);
+      res.end()
+      return
+    }
+    console.log("Users fetched successfully!");
+    // res.json(rows);
+
+    res.render('welcome', {clients: rows});
+  })*/
+
+  res.render('homepage')
+});
+
+router.get('/clients', function(req, res, next) {
+
+    const connection = mysql.createConnection({
     host: 'us-cdbr-iron-east-02.cleardb.net',
     user: 'b525fb3beb7abe',
     database: 'heroku_f88e5c34407b619',
