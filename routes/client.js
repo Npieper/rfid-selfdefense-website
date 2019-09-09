fda9240d39907c871589e453853b0be13324bf22
Niklas Pieper <niklas.pieper@trustedshops.com>
2019-09-09 13:39:21 +0200

    Change navbar.

---
+++ routes/client.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql');
+
+
+
+router.get('/client', function(req, res, next) {
+
+    const connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-02.cleardb.net',
+    user: 'b525fb3beb7abe',
+    database: 'heroku_f88e5c34407b619',
+    password: '431db98c'
+  })
+
+
+  const queryString = "SELECT * FROM user";
+  connection.query(queryString, (err, rows, fields) => {
+    if(err) {
+      console.log("Failed to query for users: " + err);
+      res.end()
+      return
+    }
+    console.log("Users fetched successfully!");
+    // res.json(rows);
+
+    res.render('welcome', {clients: rows});
+  })
+});
+
+module.exports = router;
