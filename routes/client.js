05c39a52229cfdb37526d389e2b514766a863c32
Niklas Pieper <pieper.niklas.gm@gmail.com>
2019-09-09 21:33:06 +0200

    Update user with static id

--- routes/client.js
+++ routes/client.js
@@ -7,27 +7,47 @@ const mysql = require('mysql');
 router.get('/client', function(req, res, next) {
 
     const connection = mysql.createConnection({
-    host: 'us-cdbr-iron-east-02.cleardb.net',
-    user: 'b525fb3beb7abe',
-    database: 'heroku_f88e5c34407b619',
-    password: '431db98c'
-  })
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b525fb3beb7abe',
+        database: 'heroku_f88e5c34407b619',
+        password: '431db98c'
+    })
 
 
-  const queryString = "SELECT * FROM user";
-  connection.query(queryString, (err, rows, fields) => {
-    if(err) {
-      console.log("Failed to query for users: " + err);
-      res.end()
-      return
-    }
-    console.log("Users fetched successfully!");
-    res.render('welcome', {clients: rows});
-  })
+    const queryString = "SELECT * FROM user";
+    connection.query(queryString, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for users: " + err);
+            res.end()
+            return
+        }
+        console.log("Users fetched successfully!");
+        res.render('welcome', { clients: rows });
+    })
 });
 
 router.post('/client', function(req, res, next) {
-  // Hier user in Datenbank speichern
+    const connection = mysql.createConnection({
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b525fb3beb7abe',
+        database: 'heroku_f88e5c34407b619',
+        password: '431db98c'
+    })
+
+    var chip_id = req.body.chipId;
+    console.log(chip_id)
+
+
+    const queryString = "UPDATE user set vorname = \"Test\" where chip_id = 1";
+    connection.query(queryString, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to Update " + err);
+            res.end()
+            return
+        }
+        console.log("User updated successfully!");
+        res.render('homepage');
+    })
 });
 
 module.exports = router;
